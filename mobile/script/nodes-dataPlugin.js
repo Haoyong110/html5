dddcb0f34bb81d41d9243bcf680a04e2e23042f1
Haoyong110 <haoyong94520@outlook.com>
2015-12-23 15:28:02 +0800

    修改version 1.0 nodes-dataPlugin.html

--- mobile/script/nodes-dataPlugin.js
+++ mobile/script/nodes-dataPlugin.js
@@ -69,8 +69,6 @@ $(function() {
 	$("table").append("<tr><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th>" + 
 		"<th>Fri</th><th>Sat</th><th>Sun</th></tr>");
 	for(var i = 0; i < 6; i++) {
-		if((i > 3) && 
-			break;
 		$("table").append(row());
 	}
 
